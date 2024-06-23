9d2e9b3b676a42b854b4ba98f5570c616762874c
rakib-hasan-78 <rakib.webdev78@gmail.com>
2024-06-23 18:59:35 +0600

    nav completed

--- main.js
+++ main.js
@@ -2,3 +2,13 @@ import './src/css/style.css';
 import './src/js/footer';
 import './src/js/theme';
 import './src/js/tab';
+
+
+const hamBurger = document.getElementById('hamburger');
+const mobileMenu = document.getElementById('mobile-menu');
+
+hamBurger.addEventListener('click', (e)=>{
+    hamBurger.classList.toggle('open');
+    mobileMenu.classList.toggle('hidden');
+    mobileMenu.classList.toggle('flex');
+});
