fcc890d2e2f7b1a246e44d31c47af253b5bb824d
rakib-hasan-78 <rakib.webdev78@gmail.com>
2024-06-23 12:26:48 +0600

    footer completed

--- main.js
+++ main.js
@@ -87,3 +87,12 @@ const tabIndex = localStorage.getItem('tabIndex')
             document.getElementById('data').getElementsByClassName(classString)[0].classList.add('flex');
         })
 })
+
+
+// footer 
+document.getElementById('copy-right').innerHTML=`
+    &copy; ${new Date().getFullYear()} All The Rights Reserved.
+`
+document.getElementById('credit').innerHTML=`
+    Designed By <span id="c-name" class="duration-200 ease-in-out transition hover:text-softRed cursor-pointer font-semibold tracking-widest">Rakib Hasan</span>.
+`
